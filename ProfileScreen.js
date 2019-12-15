57723821c9c208cab69fe4902db8015764c9dcaa
jinit <jinit2000@yahoo.com>
2019-12-15 22:06:40 +0530

    first commit

---
+++ ProfileScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View } from 'native-base'
+
+export class ProfileScreen extends Component {
+    render() {
+        return (
+           <View>
+               <Text>
+                   Hello
+               </Text>
+           </View>
+        )
+    }
+}
+
+export default ProfileScreen
