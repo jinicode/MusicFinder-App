2e6fa0bc4dfa26176253bf7e1305fc9000516b8a
Jinit-Jain <jinit2000@yahoo.com>
2019-12-18 21:13:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -41,48 +41,6 @@ export default ()=>{
  return <App/>
 }
 
-// import React from 'react';  
-// import { StyleSheet, Text, View } from 'react-native';  
-// import {  createAppContainer } from 'react-navigation';  
-//   import {createBottomTabNavigator} from 'react-navigation-tabs'
-// class Finder extends React.Component {  
-//     render() {  
-//         return (  
-//             <View style={styles.container}>  
-//                 <Text>Home Screen</Text>  
-//             </View>  
-//         );  
-//     }  
-// }  
-// class ProfileScreen extends React.Component {  
-//     render() {  
-//         return (  
-//             <View style={styles.container}>  
-//                 <Text>Profile Screen</Text>  
-//             </View>  
-//         );  
-//     }  
-// }  
-  
-// const TabNavigator = createBottomTabNavigator({  
-//     Home: Finder,  
-//     Profile: ProfileScreen,  
-// },
-// {  
-//     initialRouteName: "Home"  
-// }  
-// );  
-// const styles = StyleSheet.create({  
-//     container: {  
-//         flex: 1,  
-//         justifyContent: 'center',  
-//         alignItems: 'center'  
-//     },  
-// });  
-  
-// const App= createAppContainer(TabNavigator); 
-// export default ()=>{
-//  return <App/>
-// }
+
 
 
